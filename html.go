9d13013fc5e589a2978c7573b633c87331444b49
J. Lucas Café <106561488+lucascafeee@users.noreply.github.com>
2023-05-03 15:59:12 -0300

    Adicionando arquivo que testa as rotas fazendo requisição de uma pagina html

---
+++ html.go
@@ -0,0 +1,34 @@
+package main
+
+// Esse arquivo esta chamando o arquivo view.html. No caso, fiz uma rota para que chame a pagina html e exiba na rota respectivamente.
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var templates *template.Template
+
+type usuarios struct {
+	Nome  string
+	Email string
+}
+
+func main() {
+
+	templates = template.Must(template.ParseGlob("*.html"))
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+
+		u := usuarios{
+			"João",
+			"asgdvasd@gmail.com",
+		}
+
+		templates.ExecuteTemplate(w, "view.html", u)
+	})
+
+	fmt.Println("Rodando na porta 8080")
+	http.ListenAndServe(":8080", nil)
+}
